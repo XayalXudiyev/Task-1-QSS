bf3acab3399ff6fda5a3074da72a8ae9ab8204b2
XayalXudiyev <xeyalxeyal068@gmail.com>
2023-12-28 11:16:04 +0400

    Refactor select dropdowns in App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,44 +2,55 @@ import React from 'react'
 
 export default function App() {
   return (
-    <div className='bg-[#051124] h-screen m-0 p-0 '>
+    <div className='bg-[#051124] h-screen'>
 
-      <ul className='flex flex-wrap justify-between container mx-auto' >
+      <div className='container flex justify-between mx-auto'>
 
-        <li className='border border-[#4A628A] bg-[#293F64] my-8 p-2 rounded-lg text-white w-[202px] h-[38px] flex items-center text-[14px]'>
-          <select name="" id="" className=' bg-transparent w-full focus:outline-none'>
-            <option value="" className='option'>(All)</option>
-            <option value="" className='option'>(All)</option>
-            <option value="" className='option'>(All)</option>
+        <div className="relative  ">
+          <h1 className='text-[#A7B4CA] my-2'>Country</h1>
+          <select className="w-full p-2.5 text-[#A7B4CA] bg-[#293F64] border border-[#4A628A]  shadow-sm outline-none appearance-none  rounded-[7px]">
+            <option>(All)</option>
+            <option>Laravel 9 with React</option>
+            <option>React with Tailwind CSS</option>
+            <option>React With Headless UI</option>
           </select>
-        </li>
+        </div>
 
-        <li className='border border-[#4A628A] bg-[#293F64] my-8 p-2 rounded-lg text-white w-[202px] h-[38px] flex items-center'>
-          <select name="" id="" className=' bg-transparent w-full  focus:outline-none '>
-            <option value="" className='option bg-transparent rounded-full '>Economy</option>
-            <option value="" className='option'>(Economy)</option>
-            <option value="" className='option'>(Economy)</option>
+        <div className="relative ">
+          <h1 className='text-[#A7B4CA] my-2'>Sector</h1>
 
+          <select className="w-full p-2.5 text-[#A7B4CA] bg-[#293F64] border border-[#4A628A]  shadow-sm outline-none appearance-none rounded-[7px]">
+            <option>Economy</option>
+            <option>Laravel 9 with React</option>
+            <option>React with Tailwind CSS</option>
+            <option>React With Headless UI</option>
           </select>
-        </li>
+        </div>
 
-        <li className='border border-[#4A628A] bg-[#293F64] my-8 p-2 rounded-lg text-white w-[202px] h-[38px] flex items-center'>
-          <select name="" id="" className=' bg-transparent w-full focus:outline-none'>
-            <option value="" className='option'>GDP and economic gr..</option>
-            <option value="" className='option'>GDP and economic gr..</option>
-            <option value="" className='option'>GDP and economic gr..</option>
+
+        <div className="relative ">
+          <h1 className='text-[#A7B4CA] my-2'>Subsector</h1>
+
+          <select className="w-full p-2.5 text-[#A7B4CA] bg-[#293F64] border border-[#4A628A]  shadow-sm outline-none appearance-none rounded-[7px]">
+            <option>GDP and economic gr..</option>
+            <option>Laravel 9 with React</option>
+            <option>React with Tailwind CSS</option>
+            <option>React With Headless UI</option>
           </select>
-        </li>
+        </div>
 
-        <li className='border border-[#4A628A] bg-[#293F64] my-8 p-2 rounded-lg text-white w-[202px] h-[38px] flex items-center'>
-          <select name="" id="" className=' bg-transparent w-full focus:outline-none'>
-            <option value="" className='option'>Gross Domestic Prod..</option>
-            <option value="" className='option'>Gross Domestic Prod..</option>
-            <option value="" className='option'>Gross Domestic Prod..</option>
+
+        <div className="relative ">
+          <h1 className='text-[#A7B4CA] my-2'>Indicator</h1>
+
+          <select className="w-full p-2.5 text-[#A7B4CA] bg-[#293F64]  border border-[#4A628A] shadow-sm outline-none appearance-none rounded-[7px]">
+            <option>Gross Domestic Prod..</option>
+            <option>Laravel 9 with React</option>
+            <option>React with Tailwind CSS</option>
+            <option>React With Headless UI</option>
           </select>
-        </li>
-
-      </ul>
+        </div>
+      </div>
     </div>
   )
 }
